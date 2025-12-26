483db96920a696172e7b8ce411869534b6e4606d
femtilapp-jpg <femtilapp@gmail.com>
2025-12-26 16:37:54 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
-basic.forever(function () {
-	
-})
+namespace KSIR {
+    //% block="koble IR mottaker til pin %pin"
+    //% pin.defl=DigitalPin.P0
+    //% weight=100
+    export function connect(pin: DigitalPin): void {
+        // Dette kaller den underliggende IR-utvidelsen
+        IrRemote.connectIRReceiver(pin)
+    }
+
+    //% block="siste IR kode"
+    //% weight=90
+    export function lastCode(): number {
+        return IrRemote.irButton()
+    }
+
+    //% block="når IR kode %code mottas"
+    //% code.defl=1
+    //% weight=80
+    export function onCode(code: number, handler: () => void): void {
+        control.inBackground(() => {
+            let last = -1
+            while (true) {
+                const c = IrRemote.irButton()
+                if (c !== 0 && c !== last) {
+                    last = c
+                    if (c === code) handler()
+                }
+                basic.pause(10)
+            }
+        })
+    }
+}
